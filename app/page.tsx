e8ad45a52dfe916aa9a82b7f3c6772cbb193730c
Nova <aelithron@gmail.com>
2025-12-31 22:27:48 -0700

    homepage + font awesome

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8 md:p-16">
-      <h1>Journal</h1>
+      <h1 className="text-3xl font-semibold"><FontAwesomeIcon icon={faPencil} /> Journal</h1>
+      <p>A social journaling app based around the calendar!</p>
     </main>
   );
 }
