dc526bedc6e58ae7e9f7915e6ed5ef98c1aaea7a
Nova <aelithron@gmail.com>
2026-01-10 15:08:01 -0700

    improve error handling, start work on /edit page

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,4 +1,49 @@
-export default async function Page() {
+import { auth, signOut } from "@/utils/auth"
+import db from "@/utils/db"
+import { journalTable } from "@/utils/schema"
+import { faHome, faSignOut, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { and, eq } from "drizzle-orm"
+import Link from "next/link";
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const session = await auth();
+  if (!session?.user?.email) return (
+    <main className="flex flex-col min-h-screen p-8 md:p-16">
+      <div className="flex flex-col p-2 bg-red-600 rounded-xl">
+        <h1 className="text-xl"><FontAwesomeIcon icon={faX} /> Error</h1>
+        <p>Your email isn&apos;t valid, please sign out and back in!</p>
+        <form action={async () => {
+          "use server";
+          await signOut();
+        }}>
+          <button type="submit" className="bg-slate-500 p-1 px-2 rounded-xl hover:text-sky-500 mt-2"><FontAwesomeIcon icon={faSignOut} /> Sign Out</button>
+        </form>
+      </div>
+    </main>
+  );
+  let entry;
+  try {
+    entry = await db.select().from(journalTable).where(and(eq(journalTable.user, session?.user?.email), eq(journalTable.id, Number.parseInt((await params).id)))).limit(1);
+  } catch {
+    return (
+      <main className="flex flex-col min-h-screen p-8 md:p-16">
+        <div className="flex flex-col p-2 bg-red-600 rounded-xl">
+          <h1 className="text-xl"><FontAwesomeIcon icon={faX} /> Error</h1>
+          <p>Couldn&apos;t connect to the database!</p>
+        </div>
+      </main>
+    )
+  }
+  if (!entry || entry.length < 1) return (
+    <main className="flex flex-col min-h-screen p-8 md:p-16">
+      <div className="flex flex-col p-2 bg-red-600 rounded-xl">
+        <h1 className="text-xl"><FontAwesomeIcon icon={faX} /> Error</h1>
+        <p>This entry doesn&apos;t exist!</p>
+        <Link href={"/home"} className="bg-slate-500 p-1 px-2 rounded-xl hover:text-sky-500 w-fit mt-2"><FontAwesomeIcon icon={faHome} /> Go Home</Link>
+      </div>
+    </main>
+  );
   return (
     <main className="flex flex-col min-h-screen p-8 md:p-16">
 
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,14 +8,12 @@ import { journalTable } from "@/utils/schema";
 import { eq } from "drizzle-orm";
 import { CreateEntry } from "./entry.module";
 import Link from "next/link";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faX } from "@fortawesome/free-solid-svg-icons";
 
 export const metadata: Metadata = { title: "Home" }
 export const dynamic = "force-dynamic";
 export default async function Page() {
   const session = await auth();
-  const allEntries = await db.select().from(journalTable).where(eq(journalTable.user, session?.user?.email as string));
-  const entries = allEntries.filter((entry) => entry.createdAt.toDateString() === new Date().toDateString());
   return (
     <main className="flex flex-col min-h-screen p-8 md:p-16">
       <div className="flex justify-between gap-2 items-center">
@@ -26,19 +24,37 @@ export default async function Page() {
         <CreateEntry />
       </div>
       <h2 className="text-xl font-semibold mt-2">Your Entries</h2>
-      <div className="flex mt-2">
-        {entries.length < 1 && <div className="flex justify-between gap-4 items-center p-2 bg-slate-300 dark:bg-slate-700 rounded-xl">
-          <p>You haven&apos;t created any entries today!</p>
-          <CreateEntry />
-        </div>}
-        {entries.length >= 1 && entries.map((entry) => <Link href={`/edit/${entry.id}`} key={entry.id} className="flex justify-between items-center p-2 bg-slate-300 dark:bg-slate-700 rounded-lg w-full">
-          <div className="flex flex-col">
-            <p className="text-lg">{entry.title === "" ? "New Entry" : entry.title}</p>
-            <p>{entry.body === "" ? "This entry doesn't have any text yet!" : (entry.body.length < 100 ? entry.body : `${entry.body.slice(0, 100)}...`)}</p>
-          </div>
-          <FontAwesomeIcon icon={faArrowRight} size="lg" className="hover:text-sky-500"/>
-        </Link>)}
-      </div>
+      <EntryDisplay />
     </main>
   );
+}
+async function EntryDisplay() {
+  const session = await auth();
+  let allEntries;
+  try {
+    allEntries = await db.select().from(journalTable).where(eq(journalTable.user, session?.user?.email as string));
+  } catch {
+    return (
+      <div className="flex flex-col p-2 bg-red-600 rounded-xl">
+        <h1 className="text-xl"><FontAwesomeIcon icon={faX} /> Error</h1>
+        <p>Couldn&apos;t connect to the database!</p>
+      </div>
+    );
+  }
+  const entries = allEntries.filter((entry) => entry.createdAt.toDateString() === new Date().toDateString());
+  return (
+    <div className="flex mt-2">
+      {entries.length < 1 && <div className="flex justify-between gap-4 items-center p-2 bg-slate-300 dark:bg-slate-700 rounded-xl">
+        <p>You haven&apos;t created any entries today!</p>
+        <CreateEntry />
+      </div>}
+      {entries.length >= 1 && entries.map((entry) => <Link href={`/edit/${entry.id}`} key={entry.id} className="flex justify-between items-center p-2 bg-slate-300 dark:bg-slate-700 rounded-lg w-full">
+        <div className="flex flex-col">
+          <p className="text-lg">{entry.title === "" ? "New Entry" : entry.title}</p>
+          <p>{entry.body === "" ? "This entry doesn't have any text yet!" : (entry.body.length < 100 ? entry.body : `${entry.body.slice(0, 100)}...`)}</p>
+        </div>
+        <FontAwesomeIcon icon={faArrowRight} size="lg" className="hover:text-sky-500" />
+      </Link>)}
+    </div>
+  )
 }
