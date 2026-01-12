aa9cfed193a4388c2edc0a1cb21915676ec98419
Nova <aelithron@gmail.com>
2026-01-12 01:59:50 -0700

    add autosave

--- app/edit/[id]/edit.form.tsx
+++ app/edit/[id]/edit.form.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { APIError, APISuccess, JournalEditReq } from "@/journal";
-import { faSave, faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { faCloud, faSave, faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 export function JournalEditor({ id, origTitle, origBody, origCreatedAt }: { id: number, origTitle: string, origBody: string, origCreatedAt: Date }) {
   const router = useRouter();
@@ -31,9 +32,13 @@ export function JournalEditor({ id, origTitle, origBody, origCreatedAt }: { id: 
       return;
     }
     if ((resBody as APIError).error) {
-      alert(`Error creating entry: ${(resBody as APIError).message} (${(resBody as APIError).error})`);
+      alert(`Error editing entry: ${(resBody as APIError).message} (${(resBody as APIError).error})`);
       return;
     }
+    setSaved(true);
+    setCurTitle(title);
+    setCurBody(body);
+    setCurCreatedAt(createdAt);
   }
   async function deleteEntry() {
     if (!confirm(`Are you sure you want to delete this entry?\nTitle: ${curTitle !== "" ? curTitle : "New Entry"}\nCreated On: ${new Date(curCreatedAt).toLocaleString(undefined, { timeStyle: "short", dateStyle: "long" })}`)) return;
@@ -46,23 +51,57 @@ export function JournalEditor({ id, origTitle, origBody, origCreatedAt }: { id: 
       return;
     }
     if ((resBody as APIError).error) {
-      alert(`Error creating entry: ${(resBody as APIError).message} (${(resBody as APIError).error})`);
+      alert(`Error deleting entry: ${(resBody as APIError).message} (${(resBody as APIError).error})`);
       return;
     }
     router.push("/home");
+  }
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const editBody: JournalEditReq = {};
+      if (curTitle !== title) editBody.title = title;
+      if (curBody !== body) editBody.body = body;
+      if (curCreatedAt !== createdAt) editBody.createdAt = createdAt;
+      fetch(`/api/journal/${id}`, { method: "PATCH", body: JSON.stringify(editBody) })
+        .then((res) => {
+          try {
+            return res.json() as Promise<APISuccess | APIError>;
+          } catch {
+            alert("Error editing entry!");
+            return null;
+          }
+        })
+        .then((resBody) => {
+          if ((resBody as APIError).error) {
+            alert(`Error creating entry: ${(resBody as APIError).message} (${(resBody as APIError).error})`);
+            return;
+          }
+          setSaved(true);
+          setCurTitle(title);
+          setCurBody(body);
+          setCurCreatedAt(createdAt);
+        });
+    }, 1500);
+    return () => clearTimeout(timeout);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id, title, body, createdAt]);
+  function handleSet(text: string, setter: Dispatch<SetStateAction<string>>) {
+    setter(text);
+    setSaved(false);
   }
   return (
     <form className="flex flex-col gap-2" onSubmit={editEntry}>
       <div className="flex items-center justify-between">
         <div className="flex gap-2">
           <label className="text-lg font-semibold" htmlFor="title">Title:</label>
-          <input id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="underline text-lg outline-none" placeholder="Enter a title..." />
+          <input id="title" type="text" value={title} onChange={(e) => handleSet(e.target.value, setTitle)} className="underline text-lg outline-none" placeholder="Enter a title..." />
         </div>
-        <input type="datetime-local" value={createdAt} onChange={(e) => setCreatedAt(e.target.value)} />
+        <input type="datetime-local" value={createdAt} onChange={(e) => handleSet(e.target.value, setCreatedAt)} />
       </div>
       <label htmlFor="body" className="text-lg font-semibold">Entry</label>
-      <textarea id="body" value={body} onChange={(e) => setBody(e.target.value)} rows={7} className="bg-slate-500 border-slate-700 border-2 rounded-lg p-1" />
+      <textarea id="body" value={body} onChange={(e) => handleSet(e.target.value, setBody)} rows={7} className="bg-slate-500 border-slate-700 border-2 rounded-lg p-1" />
       <div className="flex gap-2 items-center justify-center mt-2">
+        <FontAwesomeIcon icon={saved ? faCloud : faSync} className="bg-slate-500 p-2 rounded-2xl" />
         <button type="submit" className="p-1 px-2 bg-violet-500 rounded-lg hover:text-sky-500"><FontAwesomeIcon icon={faSave} /> Save</button>
         <button type="button" className="p-1 px-2 rounded-lg bg-red-600 hover:text-sky-500" onClick={deleteEntry}><FontAwesomeIcon icon={faTrashAlt} /> Delete</button>
       </div>
